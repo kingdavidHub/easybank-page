82d1d7601113eac49a98421236e8aef1b17f85b7
David Igbayilola <kingigbayilola@gmail.com>
2024-12-02 23:53:32 +0100

    Add Footer component with social links and styles; include EasyBank logo and update layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,83 @@
-import style  from './Footer.module.css'
-import classNames from 'classnames'
+import styles from "./Footer.module.css";
+import classNames from "classnames";
+import EasyBankLogo from "../../assets/logo-white.svg";
+import facebookIcon from "../../assets/icon-facebook.svg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="fWrapper">
+      <div
+        className={classNames(
+          "paddings",
+          "innerWidth",
+          "flexColCenter",
+          styles.fContainer
+        )}
+      >
+        <div className="flexColCenter">
+          <img src={EasyBankLogo} alt="Easy Bank Logo" />
+          <div className={classNames("flexCenter", styles.socialLinks)}>
+            <button>
+              <a href="#">
+                <img src={facebookIcon} alt="facebook icon" />
+              </a>
+            </button>
+            <button>
+              <a href="#">
+                <img src={facebookIcon} alt="facebook icon" className={classNames(styles.links)} />
+              </a>
+            </button>
+            <button>
+              <a href="#">
+                <img src={facebookIcon} alt="facebook icon" />
+              </a>
+            </button>
+            <button>
+              <a href="#">
+                <img src={facebookIcon} alt="facebook icon" />
+              </a>
+            </button>
+            <button>
+              <a href="#">
+                <img src={facebookIcon} alt="facebook icon" />
+              </a>
+            </button>
+          </div>
+        </div>
 
-export default Footer
+        <div className="flexColCenter">
+          <span>
+            <a href="#">About</a>
+          </span>
+          <span>
+            <a href="#">Contact</a>
+          </span>
+          <span>
+            <a href="#">Blog</a>
+          </span>
+        </div>
+
+        <div className="flexColCenter">
+          <span>
+            <a href="#">Careers</a>
+          </span>
+
+          <span>
+            <a href="#">Support</a>
+          </span>
+
+          <span>
+            <a href="#">Privacy Policy</a>
+          </span>
+        </div>
+
+        <div className="flexColCenter">
+          <button className="buttons">Request Invite</button>
+          <span className="secondaryText">&copy; Easybank. All Rights Reserved</span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
